68e46e6c9728c710f441826ff47f6f2233134326
sherouk22 <sheroukmohy558@gmail.com>
2025-02-26 00:26:25 +0200

    Add project files.

---
+++ Assignment 01_EFCore/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_01_EFCore.Models
+{
+    internal class Student
+    {
+        public int MyProperty { get; set; }
+
+    }
+}
---
+++ Assignment 01_EFCore/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_01_EFCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
